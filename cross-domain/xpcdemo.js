0e9d2e8e8f6ad35ba025a5521d7325f30b89afc9
Parker Seidel <parker@indeed.com>
2011-12-12 15:59:56 -0600

    Trying to setup cross domain examples on parker.github

---
+++ cross-domain/xpcdemo.js
@@ -0,0 +1,61 @@
+var xpcdemo = (function() {
+   var xpc = {};
+   
+   xpc._channel = null;
+   
+   xpc.listen = function() {
+       function receiveMessage(event)
+       {
+           xpc.log_msg(event.data + " | " + event.origin);
+       }
+       
+       window.addEventListener("message", receiveMessage, false);
+   };
+   
+   xpc.log_msg = function(s_msg) {
+       var log = document.getElementById('log');
+       if(!log) {
+           log = document.createElement('div');
+           log.id = 'log';
+           document.body.appendChild(log);
+       }
+       var msg = document.createElement('p');
+       msg.innerHTML = s_msg;
+       log.insertBefore(msg, log.children[0]);
+       
+   }
+   
+   xpc.createiframe = function(url) {
+       var iframe = document.createElement('iframe');
+       iframe.style.cssText = 'width: 50%; height: 100%; float: right;'
+       document.body.appendChild(iframe);
+       iframe.src = url;
+       
+       xpc._channel = iframe.contentWindow;
+   };
+   
+   xpc.setup_outer = function() {
+      xpc.createiframe(document.location.href.replace('local.dev.html', 'local.cdn.html'));
+      //xpc.createiframe(document.location.href.replace('local.dev', 'local.cdn', 'g'));  
+      xpc.listen();
+      xpc.log_msg(document.location.href);
+   };
+      
+   xpc.setup_inner = function() {
+       xpc._channel = window.parent;
+       xpc.listen();
+       xpc.log_msg(document.location.href);
+   };
+   
+   xpc.send = function(msg) {
+       xpc._channel.postMessage(msg, "*")
+   };
+   
+   xpc.reset_domain = function() {
+       // go from www.at.long.local.dev to local.dev
+       var old_domain = document.domain;
+       document.domain = document.domain.split(".").slice(-2).join('.');
+       xpc.log_msg('Changed domain from : ' + old_domain + ' to ' + document.domain);
+   };
+   return xpc;
+}());
